d17d35f9cde12b1939a31202ee5170e23343b0b5
baohuu <baohuu752@gmail.com>
2026-02-04 23:32:10 +0700

    test1

--- WebApp/MyApp/admin.py
+++ WebApp/MyApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
